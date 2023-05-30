965c573291e9045eb2c22d96bfdf44b0a9d4c717
jairoBerdugo <jairoberdugo@hotmail.es>
2023-05-30 01:36:11 -0500

    ejercicio01.js palindromo entendiendo una funcion en un if(next_palm(variable.toString()))

--- ejercicio01.js
+++ ejercicio01.js
@@ -1,36 +1,86 @@
-function esPalindromo(cadena){
-    let array = cadena.split("");
-    let reverse = array.reverse("");
-//while(cadena != reverse.join("")){
-    for(let i = cadena; i<cadena == reverse.join(""); i++){
-    return cadena == reverse.join("") ? "si es palindromo" : "no es pali "+ i++
-}
-}
-//}
-console.log(esPalindromo("182"))
+function iterar(numero) {
+    numero++; // porque incrementar el número en 1 desde aqui???
+    while (true) {//PARTE QUE QUIERO ENTENDER!!!
+        if (next_pal(numero.toString())) { //llama a la funcion next_palm() y le pasa como argumento una representacion en cadena del numero actual
+        console.log(numero)
+            return numero;
+        }
+        numero++;
+    console.log(numero)
+    }
+  }
+  
+  function next_pal(numero) {
+    let pal = numero.split("");
+    let alreves = pal.reverse("");
+    let juntar = alreves.join("")
+
+    console.log(juntar)
+    return numero === juntar;
+  }
+  
+  // Ejemplo de uso
+  let numero = 333
+  let funPal = iterar(numero);
+  console.log(funPal);
+
+
 
 /*function esPalindromo(cadena){
     let array = cadena.split("");
     let reverse = array.reverse("");
-    for(i=cadena; cadena=reverse.join(""); i++){
+while(cadena != reverse.join("")){
+    return cadena == reverse.join("") ? "si es palindromo" : "no es pali "+ i++
+}
+
+}
+console.log(esPalindromo("181"))*/
+
+
+
+/*function esPalindromo(cadena){
+    let array = cadena.split("");
+    let reverse = array.reverse("");
     return cadena = reverse.join("")
+
     }
 
-i++
-}
+
 console.log(esPalindromo("181"))
 console.log(i)*/
 
+/*function iterar(num) {
+  num++; // Incrementar el número en 1
+    while (true) {
+        if (next_pal(num.toString())) { // Verificar si el número es palíndromo
+        console.log(num)
+            return num;
+        }
+    num++;
+    console.log(num)
+    }
+  }
+
+function next_pal(numero){
+    let pal = numero.split("");
+    let alreves = pal.reverse("");
+    let juntar = alreves.join("")
+    return numero == juntar
+    
+}
+
+console.log(next_pal("182"))*/
 /*function next_pal(numero){
     let pal = numero.split("");
     let alreves =pal.reverse("");
+    let juntar = alreves.join("")
     while(numero != alreves.join("")){
         if(numero 1= alreves.join("")){
             console.log(numero)
             numero++
 
         }
-        else if(numero == alreves.join("")){
+        else if(numero == juntar){
             console.log(numero)
 
         }
@@ -66,4 +116,4 @@ console.log(next_pal(188))*/
         }
     }
     mostrarAtaques(ataques)
-}*//
+}*/
